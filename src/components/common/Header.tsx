def91ee1d6cb25ca57a1d0b1e77278357ab39b80
chysis <chysiss@naver.com>
2024-11-23 01:38:44 +0900

    design: change header component to sticky

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="top-0 z-10 w-full h-20 bg-slate-700 p-4">
+    <header className="sticky top-0 z-10 w-full h-16 bg-slate-700 p-2">
       <div className="flex">
         <Link to={'/'}>
           <span className="text-5xl font-extrabold text-white">AD</span>
